79860bdf53d2d416e6ad07880ccce67b2397537b
walter <iamwalteh@me.com>
2023-06-12 17:13:38 -0500

    fix buildx tags

--- internal/docker/tags.go
+++ internal/docker/tags.go
@@ -8,11 +8,11 @@ import (
 func BuildXTagString(repo string, tag string) string {
 	ismain := strings.Contains(repo, "main")
 	str := ""
-	str += "type=ref,event=branch\n"
-	str += fmt.Sprintf("type=semver,pattern=v{{version}},value=%s\n", tag)
-	str += "type=sha\n"
-	str += fmt.Sprintf("type=raw,value=latest,enable=%v\n", ismain)
-	str += fmt.Sprintf("type=semver,pattern=v{{major}}.{{minor}},value=%s,enable=%v\n", tag, ismain)
+	str += "type=ref,event=branch\\n"
+	str += fmt.Sprintf("type=semver,pattern=v{{version}},value=%s\\n", tag)
+	str += "type=sha\\n"
+	str += fmt.Sprintf("type=raw,value=latest,enable=%v\\n", ismain)
+	str += fmt.Sprintf("type=semver,pattern=v{{major}}.{{minor}},value=%s,enable=%v\\n", tag, ismain)
 	str += fmt.Sprintf("type=semver,pattern=v{{major}},value=%s,enable=%v", tag, ismain)
 	return str
 }
