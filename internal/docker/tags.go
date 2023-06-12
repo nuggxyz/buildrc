34cd9a591b5ef2160e16d39373254dd6c3bf7086
walter <iamwalteh@me.com>
2023-06-12 17:51:45 -0500

    add v to value

--- internal/docker/tags.go
+++ internal/docker/tags.go
@@ -13,8 +13,22 @@ func BuildXTagString(ctx context.Context, repo string, tag string) (string, erro
 	str += fmt.Sprintf("type=semver,pattern=v{{version}},value=%s|", tag)
 	str += "type=sha|"
 	str += fmt.Sprintf("type=raw,value=latest,enable=%v|", ismain)
-	str += fmt.Sprintf("type=semver,pattern=v{{major}}.{{minor}},value=%s,enable=%v|", tag, ismain)
-	str += fmt.Sprintf("type=semver,pattern=v{{major}},value=%s,enable=%v", tag, ismain)
+	str += fmt.Sprintf("type=semver,pattern=v{{major}}.{{minor}},value=v%s,enable=%v|", tag, ismain)
+	str += fmt.Sprintf("type=semver,pattern=v{{major}},value=v%s,enable=%v", tag, ismain)
 
 	return string(str), nil
 }
+
+// type=ref,event=branch
+//   type=semver,pattern=v{{version}},value=
+//   type=sha
+//   type=raw,value=latest,enable=true
+//   type=semver,pattern=v{{major}}.{{minor}},value=,enable=true
+//   type=semver,pattern=v{{major}},value=,enable=true
+
+//   type=ref,event=branch
+//   type=semver,pattern=v{{version}},value=0.1.0
+//   type=sha
+//   type=raw,value=latest,enable=false
+//   type=semver,pattern=v{{major}}.{{minor}},value=0.1.0,enable=false
+//   type=semver,pattern=v{{major}},value=0.1.0,enable=false
