35378f1dd784c91c2c8b8b1a8150332789e98cf1
walter <iamwalteh@me.com>
2023-06-16 15:45:56 -0500

    action setup cache proper

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -2,6 +2,8 @@ package cache
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 
 	"github.com/google/go-github/v53/github"
 	"github.com/nuggxyz/buildrc/internal/kvstore"
@@ -12,11 +14,21 @@ const (
 )
 
 func SaveRelease(ctx context.Context, name string, r *github.RepositoryRelease) error {
-	return kvstore.Save(ctx, CACHE_DIR+"/cache.db", "cache", name, r)
+
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	return kvstore.Save(ctx, filepath.Join(dir, CACHE_DIR, "cache.db"), "cache", name, r)
 }
 
 func LoadRelease(ctx context.Context, name string) (*github.RepositoryRelease, error) {
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
 	var r github.RepositoryRelease
-	_, err := kvstore.Load(ctx, CACHE_DIR+"/cache.db", "cache", name, &r)
+	_, err = kvstore.Load(ctx, filepath.Join(dir, CACHE_DIR, "cache.db"), "cache", name, &r)
 	return &r, err
 }
