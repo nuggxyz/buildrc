012b7e14a7252ebeef278605875c777eee41d2bd
walter <iamwalteh@me.com>
2023-06-18 09:22:53 -0500

    giv us the command

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ OUTPUT_FILE=$1
 export CGO_ENABLED=0
 export GO111MODULE=on
 
-echo "🚀 building $OUTPUT_FILE with ${GO_LDFLAGS}"
+echo "🚀 building $OUTPUT_FILE with .... go build -pgo=auto -v -installsuffix cgo -ldflags \"${GO_LDFLAGS}\" -o \"$OUTPUT_FILE\" \"./cmd\""
 
 go build -pgo=auto -v -installsuffix cgo -ldflags "${GO_LDFLAGS}" -o "$OUTPUT_FILE" "./cmd"
 
-$OUTPUT_FILE version
+$OUTPUT_FILE version || echo "not a valid binary"
 
 if [ ! -f "$OUTPUT_FILE" ]; then
 	echo "❌ build failed: $OUTPUT_FILE not found"
