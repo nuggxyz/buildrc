9f399845a8d830ec5d815b972b8d89be12227f57
walter <iamwalteh@me.com>
2023-08-13 05:57:31 -0500

    fix

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,13 @@
 
 FROM golang:latest AS builder
 
-# COPY . /exec
-
 COPY . /exec
 
-# RUN cd /exec && GOOS=$(go env GOOS) GOARCH=$(go env GOARCH) go build -pgo=auto -v -installsuffix cgo -ldflags "-s -w" -o /bin/main "./cmd"
 RUN GOOS=$(go env GOOS) && GOARCH=$(go env GOARCH) && /exec/buildrc-${GOOS}-${GOARCH} /bin/main
 
 FROM alpine:latest
 
 COPY --from=builder /bin/main /main
-# COPY --from=tester /exec/test-status /test-status
 
 ENTRYPOINT ["/main"]
 
--- pre-build.sh
+++ pre-build.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-OUTPUT_FILE_OVERRIDE=$1
-
 arches=$(echo "$BUILDRC_PACKAGE_JSON" | jq -r '.arch[]')
 oses=$(echo "$BUILDRC_PACKAGE_JSON" | jq -r '.os[]')
-
-echo "📦 building $BUILDRC_PACKAGE_NAME for "
 
 function ROLL() {
 
@@ -13,7 +9,7 @@ function ROLL() {
 	local arch=$2
 	local output_file=${3:-"$BUILDRC_PACKAGE_NAME-$os-$arch"}
 
-	echo "🚀 building $BUILDRC_PACKAGE_NAME for arch='$arch' os='$os'"
+	echo "📦 building $BUILDRC_PACKAGE_NAME for arch='$arch' os='$os'"
 
 	GO111MODULE=on CGO_ENABLED=0 GOOS=$os GOARCH=$arch \
 		go build -pgo=auto -v -installsuffix cgo -ldflags "-s -w" -o "$output_file" "./cmd"
@@ -26,17 +22,19 @@ function ROLL() {
 		echo "✅ build succeeded: $output_file"
 	fi
 
+	echo "🚀 moving $output_file to $BUILDRC_TARGZ"
 	cp -r "$output_file" "$BUILDRC_TARGZ"
+	echo "🚀 moving $output_file to $BUILDRC_TARGZ"
 	cp -r "$output_file" "$BUILDRC_SHA256"
 }
 
-if [ -n "$OUTPUT_FILE_OVERRIDE" ]; then
-	ROLL "$(go env GOOS)" "$(go env GOARCH)" "$OUTPUT_FILE_OVERRIDE"
+if [ -z "$BUILDRC_PACKAGE_NAME" ]; then
+	echo "⚠️ BUILDRC_PACKAGE_NAME is no set ($BUILDRC_PACKAGE_NAME), building for exec override"
+	ROLL "$(go env GOOS)" "$(go env GOARCH)" "$BUILDRC_EXEC_OVERRIDE"
 	exit 0
 else
 	for os in $oses; do
 		for arch in $arches; do
-
 			ROLL "$os" "$arch"
 		done
 	done
