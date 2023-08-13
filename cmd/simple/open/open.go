a2468cc5f4c59ea0b65c1d4cdf74cfa3cf4ba24e
walter <iamwalteh@me.com>
2023-08-13 15:33:36 -0500

    revert

--- cmd/simple/open/open.go
+++ cmd/simple/open/open.go
@@ -3,34 +3,32 @@ package open
 import (
 	"context"
 
-	"github.com/Masterminds/semver/v3"
 	"github.com/nuggxyz/buildrc/internal/common"
+	"github.com/nuggxyz/buildrc/internal/git"
 	"github.com/nuggxyz/buildrc/internal/pipeline"
 )
 
 type Handler struct {
+	Repo        string `flag:"repo" type:"repo:" default:""`
+	File        string `flag:"file" type:"file:" default:".buildrc"`
+	AccessToken string `flag:"token" type:"access_token:" default:""`
 }
 
 func (me *Handler) Run(ctx context.Context, prov common.Provider) (err error) {
 
-	rid, err := prov.Pipeline().GetFromEnv(ctx, "BUILDRC_RELEASE_TAG", prov.FileSystem())
+	targetSemver, err := git.CalculateNextPreReleaseTag(ctx, prov.Buildrc(), prov.Git(), prov.PR())
 	if err != nil {
 		return err
 	}
 
-	smvr, err := semver.NewVersion(rid)
-	if err != nil {
-		return err
-	}
-
-	next, err := prov.Release().TagRelease(ctx, prov.Git(), smvr)
+	next, err := prov.Release().TagRelease(ctx, prov.Git(), targetSemver)
 	if err != nil {
 		return err
 	}
 
 	err = pipeline.AddContentToEnv(ctx, prov.Pipeline(), prov.FileSystem(), "open", map[string]string{
 		"BUILDRC_RELEASE_ID":  next.ID,
-		"BUILDRC_RELEASE_TAG": smvr.String(),
+		"BUILDRC_RELEASE_TAG": targetSemver.String(),
 	})
 	if err != nil {
 		return err
