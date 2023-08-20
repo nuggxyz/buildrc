adce68d9fe38a3c1eec2f76f41bd9d53ed002cd5
walter <w@lter.ca>
2023-08-19 22:13:21 -0500

    v3

--- cmd/root/version/commit.go
+++ cmd/root/version/commit.go
@@ -165,6 +165,13 @@ func (me *Handler) Run(ctx context.Context, cmd *cobra.Command, gitp git.GitProv
 				return err
 			}
 
+			if latestHead.Major() < brc.Major {
+				latestHead, err = semver.NewVersion(strconv.FormatUint(brc.Major, 10) + ".0.0")
+				if err != nil {
+					return err
+				}
+			}
+
 			revision, err := gitp.GetCurrentShortHashFromRef(ctx, "HEAD")
 			if err != nil {
 				return err
