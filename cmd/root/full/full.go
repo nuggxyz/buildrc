9b95404e52ee06c563bef9f55b8cfaed1d2f22f4
Walter Scott <w@lter.ca>
2023-11-18 13:01:13 -0600

    bugfix i guess

--- cmd/root/full/full.go
+++ cmd/root/full/full.go
@@ -54,7 +54,7 @@ func (me *Handler) Run(ctx context.Context, cmd *cobra.Command, fls afero.Fs) er
 			return err
 		}
 
-		fs := afero.NewBasePathFs(fls, me.FilesDir)
+		fs := afero.NewBasePathFs(fls, "../"+me.FilesDir)
 
 		err = fs.MkdirAll(me.FilesDir, 0755)
 		if err != nil {
